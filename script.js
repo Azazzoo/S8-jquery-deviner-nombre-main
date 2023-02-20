80477720697a9b5142619daa0068648239744830
Azazzoo <2295082@cegepoutaouais.qc.ca>
2023-02-20 08:37:58 -0500

    creation du laboratoire 8

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-let aleatoire = Math.ceil(Math.random()*100); //le nombre à deviner
+
+$(document).ready(function (){
+    aleatoire = Math.ceil(Math.random()*100); //le nombre à deviner
+    $("button").click(function (){
+        $input = $("#essai").val();
+        if (aleatoire >= $input){
+            for (x=1; x <= $input;x++){
+                $("#"+x).remove();
+            }
+        }
+        else if (aleatoire <= $input){
+            for (x=$input; x <= 100 ;x++){
+                $("#"+x).remove();
+            }
+        }
+
+
+    });
+});
