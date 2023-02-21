7cb0f8ae3642a057175dd7993b5d13b324e68671
Azazzoo <2295082@cegepoutaouais.qc.ca>
2023-02-20 22:32:49 -0500

    terminer jquery

--- script.js
+++ script.js
@@ -1,19 +1,81 @@
 
 $(document).ready(function (){
+    let cpt = 0;
     aleatoire = Math.ceil(Math.random()*100); //le nombre à deviner
     $("button").click(function (){
-        $input = $("#essai").val();
-        if (aleatoire >= $input){
-            for (x=1; x <= $input;x++){
-                $("#"+x).remove();
-            }
-        }
-        else if (aleatoire <= $input){
-            for (x=$input; x <= 100 ;x++){
-                $("#"+x).remove();
-            }
-        }
+        if (cpt === 0){
+            $input = $("#essai").val();
+            if (aleatoire > $input){
+                cpt++;
+                for (x=1; x <= $input;x++){
+                    $("#"+x).remove();
+                }
+                $("#feedback").append("<div class='flex' id='try' ><p> Essais precedant : " + $input + "</p></div>").append("<p id='wrong' class='wrong reponse'>Nope !</p>").append("<p class='reponse' id='indice'>Trop petit</p>");
 
 
+            }
+            else if (aleatoire < $input){
+                cpt++;
+                for (x=$input; x <= 100 ;x++){
+                    $("#"+x).remove();
+                }
+                $("#feedback").append("<div id='try' class='flex' ><p>Essais precedant : " + $input + "</p></div>").append("<p id='wrong' class='wrong reponse'>Nope !</p>").append("<p class='reponse' id='indice'>Trop grand</p>");
+            }
+            else {
+                cpt++;
+                for (x=1; x <= $input - 1;x++){
+                    $("#"+x).remove();
+                }
+                for (x=+$input + 1 ; x <= 100 ;x++){
+                    $("#"+x).remove();
+                }
+                $("#"+ $input).addClass("winner");
+                $("#feedback").append("<p>Bravo tu as reussi !</p>");
+                $("#reset").removeClass("invisible")
+            }
+        }
+        else if(cpt < 9){
+            $input = $("#essai").val();
+            if (aleatoire > $input){
+                cpt++;
+                for (x=1; x <= $input;x++){
+                    $("#"+x).remove();
+                }
+                $("#try").append("<p>,&nbsp;" + $input + "</p>")
+                $("#indice").text("Trop petit")
+            }
+            else if (aleatoire < $input){
+                cpt++;
+                for (x=$input; x <= 100 ;x++){
+                    $("#"+x).remove();
+                }
+                $("#try").append("<p>,&nbsp;" + $input + " </p>")
+                $("#indice").text("Trop grand")
+            }
+            else {
+                cpt++;
+                for (x=1; x <= $input - 1;x++){
+                    $("#"+x).remove();
+                }
+                for (x=+$input + 1 ; x <= 100 ;x++){
+                    $("#"+x).remove();
+                }
+                $("#"+ $input).addClass("winner");
+                $("#wrong").remove();
+                $("#indice").remove();
+                $("#feedback").append("<p class='winner reponse' >Bravo tu as reussi !</p>");
+                $("#reset").removeClass("invisible")
+            }
+        }
+        else{
+            $("#try").append("<p>,&nbsp;" + $input + " </p>")
+            $("#wrong").text("Ah non :'( Meilleur chance la prochaine fois");
+            $("#soumettre").prop('disabled', true);
+            $("#reset").removeClass("invisible")
+
+        }
+    });
+    $("#reset").click(function() {
+        location.reload(false);
     });
 });
